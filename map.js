9b3cbb371d52520b8747cfb846f5a4a85b49936d
Song Su <stevensosong@gmail.com>
2026-05-15 23:10:51 -0700

    Update map.js

--- map.js
+++ map.js
@@ -81,7 +81,7 @@ map.on('load', async () => {
     type: 'line',
     source: 'cambridge_route',
     paint: {
-    'line-color': '#0096FF',  // A bright green using hex code
+    'line-color': '#32D400',  // A bright green using hex code
     'line-width': 5,          // Thicker lines
     'line-opacity': 0.6       // Slightly less transparent
         }   
